4402edfca50cad09b761e81e99a050a7537e8073
wordsforthewise <wordsforthewise@gmail.com>
2016-10-04 19:00:48 -0600

    FEAT: handle multiple faces and end of video

--- scrape-prez-vids/crop_video_face.py
+++ scrape-prez-vids/crop_video_face.py
@@ -2,7 +2,9 @@ import cv2
 import os
 import glob
 import imutils
+import numpy as np
 
+showVid = False # whether to display video--currently not working in ipython
 
 # set up face detection
 detector = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
@@ -15,17 +17,20 @@ vid = cv2.VideoCapture(testFile)
 
 # jump past the intro
 # http://stackoverflow.com/questions/33650974/opencv-python-read-specific-frame-using-videocapture
-#The first argument of cap.set(), number 2 defines that parameter for setting the frame selection.
-#Number 2 defines flag CV_CAP_PROP_POS_FRAMES which is a 0-based index of the frame to be decoded/captured next.
-#The second argument defines the frame number in range 0.0-1.0
-frame_no = 6000
-vid.set(0, frame_no)
+# http://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html#videocapture-get
+start_at = 0 # start the video at (milliseconds), for skipping intros
+vid.set(0, start_at)
 
 face_rect_list = []
+face_rect_times = []
+video_times = [] # for keeping track of the whole video
+# so we can get fraction with faces
 
 while True:
     # check out one frame
     (grabbed, frame) = vid.read()
+    if frame is None:
+        break
 
     # resize the frame, convert it to grayscale, and detect faces in the
     # frame
@@ -34,12 +39,28 @@ while True:
     faceRects = detector.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5,
     	minSize=(30, 30), flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
 
-    # loop over the faces and draw a rectangle around each
-    for (x, y, w, h) in faceRects:
-    	cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    if len(faceRects) < 1:
+        continue
 
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-    # if the 'q' key is pressed, stop the loop
-    if key == ord("q"):
-        break
+    rect = faceRects[0]
+
+    if len(faceRects) > 1:
+        # take largest face if detected multiple
+        sizes = [w * h for x, y, w, h in faceRects]
+        bestIdx = np.argmax(sizes)
+        rect = faceRects[bestIdx]
+
+    # store rectangle positions and times
+    face_rect_list.append(rect)
+    face_rect_times.append(vid.get(0))
+
+    x, y, w, h = rect
+    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    if showVid:
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the 'q' key is pressed, stop the loop
+        if key == ord("q"):
+            break
